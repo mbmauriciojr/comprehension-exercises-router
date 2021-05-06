d75ebf38c2b0fb59623a86568f778c6f1f7c5a49
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-06 16:21:43 -0300

    Exercise 6 - Where created a Route to Users Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
+import Users from './components/Users';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <Route exact path="/" component={ Home } />
         <Route path="/about" component={ About } />
+        <Route path="/users" component={ Users }/>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="Users">Users</Link>
